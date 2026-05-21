ef56fc6f4fd74d2fbfbb3912ea7902d70bb9dcac
AbdulAzeez Alimi <alimiazeez400@gmail.com>
2026-05-21 14:15:46 +0100

    rough push

--- src/client.tsx
+++ src/client.tsx
@@ -1,12 +1,11 @@
-import { hydrateRoot } from "react-dom/client";
+import { createRoot } from "react-dom/client";
 import { StrictMode } from "react";
 import { RouterProvider } from "@tanstack/react-router";
 import { getRouter } from "./router";
 
 const router = getRouter();
 
-hydrateRoot(
-  document.getElementById("root")!,
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <RouterProvider router={router} />
   </StrictMode>
